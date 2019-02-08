f3c94bc98042cac7ed3fd199111f722a71a6f919
Jacob Moller <jacob.moller8@hotmail.com>
2019-02-08 13:39:48 +0100

    welcome page update

--- src/SelectDish/SelectDish.js
+++ src/SelectDish/SelectDish.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './SelectDish.css';
 import Sidebar from '../Sidebar/Sidebar';
 import Dishes from '../Dishes/Dishes';
+import Header from "../Header/Header"
 
 class SelectDish extends Component {
   render() {
     return (
       <div className="SelectDish">
-        <h2>This is the Select Dish screen</h2>
-        
+        <Header>
+
+        </Header>
+
         {/* We pass the model as property to the Sidebar component */}
-        <Sidebar model={this.props.model}/>
-        <Dishes/>
+        <Sidebar model={this.props.model} />
+        <Dishes />
       </div>
     );
   }
--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -11,9 +11,27 @@ class Welcome extends Component {
 
         </Header>
 
-        <Link to="/search">
-          <button>Start planning</button>
-        </Link>
+        <div id="homeView" className="container">
+          <div id="homeScreen" className="row">
+            <div id="home" className="row-md-3">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque cursus accumsan. Ut cursus iaculis
+                augue
+                in vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan justo vel quam fermentum, eu
+                malesuada leo vestibulum. Nunc justo turpis, mattis at diam mattis, ultrices aliquam risus. Etiam id ultrices
+                neque. Fusce egestas ultrices justo, at accumsan enim pharetra non. Proin sed enim fermentum, bibendum eros vel,
+                venenatis massa. Sed blandit erat aliquet neque gravida lacinia. Sed vitae aliquam arcu, sed viverra dolor. Etiam
+                a
+                felis nulla. Fusce lacinia congue viverra. Pellentesque in ullamcorper sem.
+				        </p>
+
+              <Link to="/search">
+                <button id="newDinnerButton" typ="button" className="btn btn-warning">Create New Dinner!</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
