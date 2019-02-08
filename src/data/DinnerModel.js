bfee8a31afa8e8f4e05c27004e38b77c99799150
Jacob Moller <jacob.moller8@hotmail.com>
2019-02-08 15:05:46 +0100

    added old funcs to model

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -11,23 +11,104 @@ const DinnerModel = function () {
   var menu = [];
   var selectedDish = "";
 
+  // Observer pattern
+
+  this.addObserver = function (observer) {
+    observers.push(observer);
+  };
+
+  this.removeObserver = function (observer) {
+    observers = observers.filter(o => o !== observer);
+  };
+
+  const notifyObservers = function () {
+    observers.forEach(o => o.update());
+  };
+
+  // Set Number of Guests
   this.setNumberOfGuests = function (num) {
     if (num < 1) return;
     numberOfGuests = num;
     notifyObservers();
   };
 
+  // Get Number of Guests
   this.getNumberOfGuests = function () {
     return numberOfGuests;
   };
 
+  //Returns the dish that is on the menu for selected type 
+  this.getSelectedDish = function (type) {
+    return selectedDish;
+  }
+
+  // Set Selected Dish Id
+  this.setSelectedDishId = function (id) {
+    selectedDish = id;
+    notifyObservers();
+  }
+
+  //Returns all the dishes on the menu.
+  this.getFullMenu = function () {
+    return menu;
+  }
+
+  // Returns the price for the dish
+  this.getDishPrice = function (dish) {
+    var dishPrice = 0;
+    dishPrice += dish.pricePerServing * numberOfGuests;
+    return parseInt(dishPrice.toFixed(2));
+  }
+
+  //Returns the total price of the menu 
+  this.getTotalMenuPrice = function () {
+    menuPrice = 0;
+    for (var i = 0; i < menu.length; i++) {
+      menuPrice += this.getDishPrice(menu[i]);
+    }
+    return menuPrice;
+  }
+
+  //Add dish to menu
+  this.addDishToMenu = function (dish) {
+    menu.push(dish);
+    notifyObservers();
+  }
+
+  //Removes dish from menu
+  this.removeDishFromMenu = function (id) {
+    for (var i = 0; i < menu.length; i++) {
+      if (id = menu[i].id) {
+        var index = i;
+      }
+    }
+    menu.splice(index, 1);
+    notifyObservers();
+  }
+
   // API Calls
 
-  this.getAllDishes = function () {
-    const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search'
-    return fetch(url, httpOptions)
-      .then(processResponse)
-      .catch(handleError)
+
+  // Get all dishes from API
+  this.getAllDishesApi = function (type, filter) {
+    var filterVar = ""
+    var typeVar = ""
+    if (filter) { filterVar = filter }
+    if (type) { typeVar = type }
+    var ApiUrl = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?instructionsRequired=false&limitLicense=false&number=8&offset=0&query=${filterVar}&type=${typeVar}`
+    return fetch(ApiUrl
+      , {
+        headers: { 'X-Mashape-Key': APIkey }
+      }).then(response => response.json())
+      .then(data => data.results);
+  }
+  // Get selected dish from API
+  this.getDishApi = function (id) {
+    var ApiUrl = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information`;
+    return fetch(ApiUrl
+      , {
+        headers: { 'X-Mashape-Key': APIkey }
+      }).then(response => response.json())
   }
 
   // API Helper methods
@@ -49,19 +130,6 @@ const DinnerModel = function () {
     }
   }
 
-  // Observer pattern
-
-  this.addObserver = function (observer) {
-    observers.push(observer);
-  };
-
-  this.removeObserver = function (observer) {
-    observers = observers.filter(o => o !== observer);
-  };
-
-  const notifyObservers = function () {
-    observers.forEach(o => o.update());
-  };
 };
 
 export const modelInstance = new DinnerModel();
