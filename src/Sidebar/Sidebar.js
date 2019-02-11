45d19e2e4da4b0ef0ec81c52e4c8bf7499ae8432
Hugo B <hugo@bergqvist.com>
2019-02-11 15:42:40 +0100

    fixed add and remove functionallity of sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -5,83 +5,88 @@ import Button from '@material-ui/core/Button';
 
 class Sidebar extends Component {
 
-  constructor(props) {
-    super(props)
-    // we put on state the properties we want to use and modify in the component
-    this.handleRemoveClick = this.handleRemoveClick.bind(this);
-    this.state = {
-      numberOfGuests: this.props.model.getNumberOfGuests()
-    }
-  }
+	constructor(props) {
+		super(props)
+		// we put on state the properties we want to use and modify in the component
+		this.handleRemoveClick = this.handleRemoveClick.bind(this);
+		this.loadMenu = this.loadMenu.bind(this);
+		this.state = {
+			numberOfGuests: this.props.model.getNumberOfGuests(),
+			currentMenu: this.loadMenu()
+		}
+	}
+	// this methods is called by React lifecycle when the 
+	// component is actually shown to the user (mounted to DOM)
+	// that's a good place to setup model observer
+	componentDidMount() {
+		this.props.model.addObserver(this)
+	}
 
-  // this methods is called by React lifecycle when the 
-  // component is actually shown to the user (mounted to DOM)
-  // that's a good place to setup model observer
-  componentDidMount() {
-    this.props.model.addObserver(this)
-  }
+	// this is called when component is removed from the DOM
+	// good place to remove observer
+	componentWillUnmount() {
+		this.props.model.removeObserver(this)
+	}
 
-  // this is called when component is removed from the DOM
-  // good place to remove observer
-  componentWillUnmount() {
-    this.props.model.removeObserver(this)
-  }
+	loadMenu() {
+		let loadedMenu = this.props.model.getFullMenu().map((dish, i) =>
+		<div className="row" key={dish.title +" "+ i}>
+		 <div className="container-fluid menuItem" id={parseInt(dish.id, 10)} onClick={this.handleRemoveClick}>
+			 <div className="container-fluid col-6 dishName">{dish.title}</div>
+			 <div className="container-fluid col-6 cost">{dish.pricePerServing * this.props.model.getNumberOfGuests()}</div>
+		 </div>
+	 </div>);
 
-  // in our update function we modify the state which will
-  // cause the component to re-render
-  update() {
-    this.setState({
-      numberOfGuests: this.props.model.getNumberOfGuests()
-    })
-  }
+	 return loadedMenu
+	}
 
-  // our handler for the input's on change event
-  onNumberOfGuestsChanged = (e) => {
-    this.props.model.setNumberOfGuests(+e.target.value)
-  }
+	// in our update function we modify the state which will
+	// cause the component to re-render
+	update() {
+		let currentMenu = this.loadMenu();
+		this.setState({
+			numberOfGuests: this.props.model.getNumberOfGuests(),
+			currentMenu
+		})
+	}
 
-  handleRemoveClick = (e) => {
-    console.log("Click came from: " + e.target.getAttribute('id'))
-    //this.props.model.removeDishFromMenu(this.target.id)
-  }
+	// our handler for the input's on change event
+	onNumberOfGuestsChanged = (e) => {
+		this.props.model.setNumberOfGuests(+e.target.value)
+	}
 
-  render() {
-    let currentMenu = null;
+	handleRemoveClick = (e) => {
+		this.props.model.removeDishFromMenu(e.target.id)
+	}
 
-    currentMenu = this.props.model.getFullMenu().map((dish) =>
-      <div className="row" key={dish.title}>
-        <div className="container-fluid menuItem" id={parseInt(dish.id, 10)} onClick={this.handleRemoveClick}>
-          <div className="container-fluid col-6 dishName">{dish.title}</div>
-          <div className="container-fluid col-6 cost">{dish.pricePerServing}</div>
-        </div>
-      </div>
-    );
 
-    return (
-      <div className="container-fluid col-3 d-none d-sm-block Sidebar">
-        <h2>My dinner</h2>
+	render() {
+
+		return (
+			<div className="container-fluid col-3 d-none d-sm-block Sidebar">
+				<h2>My dinner</h2>
 				<div className="row">
 					<p>People: </p>
 					<input className="form-control col-4 peopleSelector" type='number' value={this.state.numberOfGuests} onChange={this.onNumberOfGuestsChanged} />
 				</div>
 
-        <div className="container-fluid col-12 sideBarTitles">
-          <div className="row">
-            <div className="container-fluid col-6 dishName">Dish Name</div>
-            <div className="container-fluid col-6 cost">Cost</div>
-          </div>
-        </div>
-        {currentMenu}
+				<div className="container-fluid col-12 sideBarTitles">
+					<div className="row">
+						<div className="container-fluid col-6 dishName">Dish Name</div>
+						<div className="container-fluid col-6 cost">Cost</div>
+					</div>
+				</div>
+				{this.state.currentMenu}
 
-        <div className="container-fluid btnContainer">
-          <Button component={Link} to="/overview" variant='contained' id="confirmDinnerBtn">
-            Confirm Dinner
+				<div className="container-fluid btnContainer">
+					<Button component={Link} to="/overview" variant='contained' id="confirmDinnerBtn">
+						Confirm Dinner
           </Button>
-        </div>
+				</div>
 
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default Sidebar;
